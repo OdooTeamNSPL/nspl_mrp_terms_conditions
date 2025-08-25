bc08331a55bb172ca4a7bc1500f3fa5f3e86deff
Team Odoo NSPL <odoo_team@namahsoftech.com>
2025-08-25 10:00:41 +0530

    Update __manifest__.py

--- nspl_mrp_terms_conditions/__manifest__.py
+++ nspl_mrp_terms_conditions/__manifest__.py
@@ -1,16 +1,32 @@
 {
-    "name": "MRP Terms & Conditions",
-    "version": "18.0",
-    "summary": "Configure Terms & Conditions for Manufacturing Orders",
-    "description": "Configure and print Terms & Conditions in Manufacturing Orders.",
-    "category": "Manufacturing",
-    "author": "NSPL",
-    "depends": ["mrp"],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/mrp_terms_views.xml",
-        "views/mrp_production_views.xml",
+    'name': 'MRP Terms & Conditions',
+    'version': '18.0',
+    'summary': 'Configure Terms & Conditions for Manufacturing Orders',
+    'description': """
+    Configure and print Terms & Conditions in Manufacturing Orders.
+
+    ✔ Define standard terms for manufacturing  
+    ✔ Automatically load conditions into MO 
+
+    Helps ensure compliance and consistency across production workflows.
+    """,
+    'category': 'Manufacturing',
+    'sequence': 4,
+    'author': 'Namah Softech Private Limited',
+    'website': 'http://www.namahsoftech.com',
+    'license': 'LGPL-3',
+    'support': 'support@namahsoftech.com',
+    'price': 9.99,
+    'currency': 'USD',
+    'contributors': ['Shivani Solanki'],
+    'depends': ['mrp'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/mrp_terms_views.xml',
+        'views/mrp_production_views.xml',
     ],
-    "installable": True,
-    "application": False
+    'images': ['static/description/img/banner.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
